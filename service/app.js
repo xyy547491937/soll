e1be3174619f0d3247f9028df7e56ae72f9936df
xyy547491937 <18210854820@163.com>
2024-04-27 22:43:48 +0800

    修改

--- service/app.js
+++ service/app.js
@@ -27,13 +27,20 @@ app.use((err, req, res, next) => {
   next();
 });
 
-async function invite_reward_result(address, exactReason, res, message) {
-  let rr = await selectData(address)
-  if (rr.length > 0) {
-    res.send(successResponse(Object.assign(rr[0], exactReason), message ? message : "查询成功"))
-  } else {
-    res.send(ErrorResponse("查询失败", exactReason))
-  }
+async function invite_reward_result(address) {
+  const sql = `SELECT
+  u.WalletAddress,
+  COUNT(DISTINCT pl.LinkID) AS InvitedCount,
+  SUM(pr.Points) AS RewardBalance
+FROM
+  Users u
+LEFT JOIN SharedLinks pl ON u.UserID = pl.UserID
+LEFT JOIN PointsRecords pr ON u.UserID = pr.InvitedUserID
+WHERE
+  u.UserID = (SELECT UserID FROM Users WHERE Username = ? LIMIT 1)
+GROUP BY
+  u.UserID`
+  return await executeQuery(sql, [address]);
 }
 
 
@@ -50,40 +57,35 @@ app.post('/addname', async (req, res) => {
   // 检查 连接是否在分享表中
   let linkRecord = await executeQuery("select * from SharedLinks where Link=?", [bodyData.link]);
   console.log('xxxxxxxxxxxx', linkRecord)
+  // 先查一下用户表是否有该用户
+  let userList = await executeQuery("select * from Users where WalletAddress=?", [bodyData.address]);
+  console.log('[xxxxxxxxxxxxxx', userList)
+  let UserRecord = {}
+  if (userList.length == 0) {
 
-  let UserRecord = await executeQuery("INSERT INTO Users (Username, InitialPoints,WalletAddress) VALUES (?,?,?)", [bodyData.address, 2000, bodyData.address]);
-  console.log(UserRecord)
+    UserRecord = await executeQuery("INSERT INTO Users (Username, InitialPoints,WalletAddress) VALUES (?,?,?)", [bodyData.address, 2000, bodyData.address]);
+    let UserRecordID = userList.length == 0 ? userList[0].UserID : UserRecord.insertId
+    if (linkRecord.length > 0) {
+      // 增加一条记录表据
+      let PointsRecords = await executeQuery("INSERT INTO PointsRecords (UserID, InvitedUserID, Timestamp) VALUES (?,?,?)", [linkRecord[0].UserID, UserRecordID, date]);
+      // 每当有新的积分记录时，更新用户的当前积分
+      //UPDATE Users SET CurrentPoints = CurrentPoints + 2000 WHERE UserID = [InvitingUserID];
+      await executeQuery("UPDATE PointsRecords SET CurrentPoints = CurrentPoints + 2000 WHERE UserID = ?", [UserRecordID]);
 
-  if (linkRecord.length > 0) {
-    // 增加一条记录表据
-    let PointsRecords = await executeQuery("INSERT INTO PointsRecords (UserID, InvitedUserID, Timestamp) VALUES (?,?,?)", [linkRecord[0].UserID, UserRecord.insertId, date]);
-    // 每当有新的积分记录时，更新用户的当前积分
-    //UPDATE Users SET CurrentPoints = CurrentPoints + 2000 WHERE UserID = [InvitingUserID];
-    await executeQuery("UPDATE PointsRecords SET CurrentPoints = CurrentPoints + 2000 WHERE UserID = ?", [UserRecord.insertId]);
+    } else {
+      // 不是通过分享点开的为 b 创建 分享连接 
+      let SharedLinksRecord = await executeQuery("INSERT INTO SharedLinks (UserID, Link) VALUES (?, ?)", [UserRecordID, bodyData.link]);
+    }
+
+    let result = invite_reward_result(bodyData.address)
+
+    res.send(successResponse(result, "查询成功"))
+
 
   } else {
-    // 不是通过分享点开的为 b 创建 分享连接 
-    let SharedLinksRecord = await executeQuery("INSERT INTO SharedLinks (UserID, Link) VALUES (?, ?)", [UserRecord.insertId, bodyData.link]);
+    let result = invite_reward_result(bodyData.address)
+    res.send(successResponse(result, "该用户已领取"))
   }
-
-  const sql = `SELECT
-  u.WalletAddress,
-  COUNT(DISTINCT pl.LinkID) AS InvitedCount,
-  SUM(pr.Points) AS RewardBalance,
-FROM
-  Users u
-LEFT JOIN SharedLinks pl ON u.UserID = pl.UserID
-LEFT JOIN PointsRecords pr ON u.UserID = pr.InvitedUserID
-WHERE
-  u.UserID = (SELECT UserID FROM Users WHERE Username = ? LIMIT 1)
-GROUP BY
-  u.UserID`
-  let result = await executeQuery(sql, [bodyData.address]);
-
-
-
-  res.send(successResponse(result, "查询成功"))
-
 });
 
 
