24aac05f0476536d24ea98316ce0561017901c4d
adminn123 <18210854820@163.com>
2024-04-28 10:29:12 +0800

    修改接口返回

--- service/app.js
+++ service/app.js
@@ -40,7 +40,17 @@ WHERE
   u.UserID = (SELECT UserID FROM Users WHERE Username = ? LIMIT 1)
 GROUP BY
   u.UserID`
-  return await executeQuery(sql, [address]);
+
+  if (!address) {
+    return null;
+  }
+
+  const sql2 = `SELECT * from SharedLinks WHERE userID =(select userID from users where walletAddress = ?) `
+
+  let r1 = await executeQuery(sql, [address]);
+  let r2 = await executeQuery(sql2, [address]);
+console.log(r1[0],r2);
+  return Object.assign(r1[0],r2[0])
 }
 
 
@@ -55,37 +65,49 @@ app.post('/addname', async (req, res) => {
   // req.body现在包含了请求体中的数据
   const bodyData = req.body;
   // 检查 连接是否在分享表中
-  let linkRecord = await executeQuery("select * from SharedLinks where Link=?", [bodyData.link]);
-  console.log('xxxxxxxxxxxx', linkRecord)
-  // 先查一下用户表是否有该用户
-  let userList = await executeQuery("select * from Users where WalletAddress=?", [bodyData.address]);
-  console.log('[xxxxxxxxxxxxxx', userList)
-  let UserRecord = {}
-  if (userList.length == 0) {
-
-    UserRecord = await executeQuery("INSERT INTO Users (Username, InitialPoints,WalletAddress) VALUES (?,?,?)", [bodyData.address, 2000, bodyData.address]);
-    let UserRecordID = userList.length == 0 ? userList[0].UserID : UserRecord.insertId
-    if (linkRecord.length > 0) {
-      // 增加一条记录表据
-      let PointsRecords = await executeQuery("INSERT INTO PointsRecords (UserID, InvitedUserID, Timestamp) VALUES (?,?,?)", [linkRecord[0].UserID, UserRecordID, date]);
-      // 每当有新的积分记录时，更新用户的当前积分
-      //UPDATE Users SET CurrentPoints = CurrentPoints + 2000 WHERE UserID = [InvitingUserID];
-      await executeQuery("UPDATE PointsRecords SET CurrentPoints = CurrentPoints + 2000 WHERE UserID = ?", [UserRecordID]);
-
-    } else {
-      // 不是通过分享点开的为 b 创建 分享连接 
-      let SharedLinksRecord = await executeQuery("INSERT INTO SharedLinks (UserID, Link) VALUES (?, ?)", [UserRecordID, bodyData.link]);
+  try {
+    let linkRecord = await executeQuery("select * from SharedLinks where Link=?", [bodyData.link]);
+    console.log('xxxxxxxxxxxx', linkRecord)
+    // 先查一下用户表是否有该用户
+    let userList = await executeQuery("select * from Users where WalletAddress=?", [bodyData.address]);
+    console.log('[xxxxxxxxxxxxxx', userList)
+    let UserRecord = {}
+    // 如果用户表 没有用户插入用户表
+    if (userList.length == 0) {
+      UserRecord = await executeQuery("INSERT INTO Users (Username, InitialPoints,WalletAddress) VALUES (?,?,?)", [bodyData.address, 2000, bodyData.address]);
     }
+    // 被邀请人的id
+    let UserRecordID = userList.length > 0 ? userList[0].UserID : UserRecord.insertId
+    if (linkRecord.length > 0) {
+      // 增加一条记录表据  先判断是否已经存在
+      let isExistPoint = await executeQuery("select * from PointsRecords where UserID=? and InvitedUserID=?", [linkRecord[0].UserID, UserRecordID])
+      if (isExistPoint.length == 0) {
+        await executeQuery("INSERT INTO PointsRecords (UserID, InvitedUserID, Timestamp) VALUES (?,?,?)", [linkRecord[0].UserID, UserRecordID, date]);
+        // 每当有新的积分记录时，更新用户的当前积分
+        //UPDATE Users SET CurrentPoints = CurrentPoints + 2000 WHERE UserID = [InvitingUserID];
+        await executeQuery("UPDATE PointsRecords SET CurrentPoints = CurrentPoints + 2000 WHERE UserID = ?", [UserRecordID]);
+      }
 
-    let result = invite_reward_result(bodyData.address)
+    }
+    // 不是通过分享连接进入的 生成自己的分享连接
+    // 如果已经添加 就不重复添加
+    await executeQuery("select * from SharedLinks where UserID = ?", [UserRecordID]).then(async (res) => {
+      if (res.length == 0) {
+        await executeQuery("INSERT INTO SharedLinks (UserID, Link) VALUES (?, ?)", [UserRecordID, shareLink]);
+      }
+    });
+
+    // await executeQuery("INSERT INTO SharedLinks (UserID, Link) VALUES (?, ?)", [UserRecordID, shareLink]);
+
+    let result = await invite_reward_result(bodyData.address)
 
     res.send(successResponse(result, "查询成功"))
 
 
-  } else {
-    let result = invite_reward_result(bodyData.address)
-    res.send(successResponse(result, "该用户已领取"))
+  } catch (error) {
+    res.send(ErrorResponse("查询失败", error))
   }
+
 });
 
 
